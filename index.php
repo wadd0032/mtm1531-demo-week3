c47c26c2751654e0036a3482101658faa7939241
wadd0032 <deannawadden@rogers.com>
2012-01-23 12:33:42 -0500

    added code for serverside validation

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+$errors =array();
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+var_dump($name);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {		//Check to see if the form has been submitted before validating
+	if (empty($name)) {
+		$errors['name'] = true;
+	}
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -10,8 +23,8 @@
 <body>
 	<form action="index.php" form method="post">
 		<div>
-			<label for="name">Name</label>
-			<input id="name"  name="name">
+			<label for="name">Name<?php if (isset($errors['name'])) : ?> <strong>is required</strong><?php endif; ?></label>
+			<input id="name"  name="name" value="<?php echo $name; ?>" required>
 		</div>
 		<div>
 			<label for="email">E-mail Address</label>
